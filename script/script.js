df38dd0ee20aa777e9ea79acd109c5a9bb6a9782
Calvin Cheng <calvincheng919@gmail.com>
2021-04-19 08:59:13 -0700

    animalfarm.updated.with.cat

--- script/script.js
+++ script/script.js
@@ -22,6 +22,16 @@ function Animal(animalName) {
 Animal.prototype.render = function() {
   console.log("this function has been called")
   // do the render thing... put yourself on the html screen
+
+  // i need to create this html: <div class="catObject">Cat</div>
+
+  let catDiv = document.createElement('div');
+  catDiv.className = 'catObject';
+  catDiv.textContent = 'Cat';
+
+  // how to finally render on html
+  // this is an animal functin... why are we specifically using cat?
+
 }
 
 //TODO: for Monday
@@ -30,4 +40,25 @@ Animal.prototype.render = function() {
 // - attach a listener to listen to the 'click'
 // - I want to respond to the click with my own list of things to do (create and render the cat)
 
+//Step by Step
+// 1. Target the cat button
 
+let makeCatButton = document.getElementById('makeCat');
+
+// 2. Attach an event listener to listen to the 'click' of cat button
+
+// anonymous function
+// on a click, please run/invoke the function - put () around at the end
+
+makeCatButton.addEventListener('click', goMakeCat)
+
+// 3. Respond to the click by console.log('cat was clicked')
+
+function goMakeCat() {
+  console.log('cat was clicked')
+
+  // target the item that cat object is to attach to
+  // create the cat object 
+  // you want to call the cat render(), and render should put the cat on the html screen
+
+}
