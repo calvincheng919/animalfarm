d7aadf45e9dfab2e797e5f0fdde22721063c97ee
Calvin Cheng <calvincheng919@gmail.com>
2021-04-21 09:01:31 -0700

    updated.04.21.2021

--- script/script.js
+++ script/script.js
@@ -1,51 +1,33 @@
 'use strict'
 
-console.log("I am here");
-
-
-//TODO: I want to create a template - constructor function for an animal
-// Is that four different constructors?
-
-// Create a single constructor - template
-// the constructor takes as parameters - animalName
-// property called - animalName
-// method to render itself on the screen - render()
-
-// Animal - done
-// takes one parameter - animalName - done
-// has one method - render() - method is simply a function that is attached to an object. 
-
+// Create a constructor function as a template for animals I want to create
+// constructor takes one parameter of animalName and assigns it to the property animalName
 function Animal(animalName) {
   this.animalName = animalName;
 }
 
+// render tells the specific animal to put itself on the screen
 Animal.prototype.render = function() {
   console.log("this function has been called")
-  // do the render thing... put yourself on the html screen
-
-  // i need to create this html: <div class="catObject">Cat</div>
-
   let animalDiv = document.createElement('div');
   animalDiv.className = `${this.animalName}Object`;
   animalDiv.textContent = this.animalName;
-  let farm = document.querySelector('#farm')
-  farm.appendChild(animalDiv);
+  let animalSection = document.querySelector(`#${this.animalName}Section`)
+  animalSection.appendChild(animalDiv);
 }
 
 //make cat
+
 let makeCatButton = document.getElementById('makeCat');
 makeCatButton.addEventListener('click', goMakeCat)
-
 function goMakeCat() {
   console.log('cat was clicked')
   let cat = new Animal('cat');
   cat.render();
 }
-
 //make dog
 let makeDogButton = document.getElementById('makeDog');
 makeDogButton.addEventListener('click', goMakeDog)
-
 function goMakeDog() {
   console.log('dog was clicked')
   let dog = new Animal('dog');
@@ -54,10 +36,24 @@ function goMakeDog() {
 
 // make horse
 
+let makeHorseButton = document.getElementById('makeHorse');
+makeHorseButton.addEventListener('click', goMakeHorse)
+function goMakeHorse() {
+  console.log('horse was clicked')
+  let horse = new Animal('horse');
+  horse.render();
+}
 
 // make sheep
 
+let makeSheepButton = document.getElementById('makeSheep');
+makeSheepButton.addEventListener('click', goMakeSheep)
 
+function goMakeSheep() {
+  console.log('sheep was clicked')
+  let sheep = new Animal('sheep');
+  sheep.render();
+}
 
 
 
