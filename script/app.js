ce1ba32f30ed6e7588f14a7dd0f802c766709323
Calvin Cheng <calvincheng919@gmail.com>
2021-04-26 08:56:39 -0700

    journal.start

---
+++ script/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// console.log('journal page is alive');
+
+// document.getElementById..., document.QuerySelector...
+
+//Target the form.
+
+
+const form = document.getElementsByTagName('FORM')[0];
+
+function makeNewArticle() {
+
+// Make a new article and insert this article on to the webpage.
+
+
+}
